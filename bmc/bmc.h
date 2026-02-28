8d1d2ac53e82b25f6e5ec41edefcd04bcba46a4c
nsaspook <nsaspook@nsaspook.com>
2026-02-27 16:24:57 -0800

    up

--- bmc/bmc.h
+++ bmc/bmc.h
@@ -36,7 +36,7 @@ extern "C" {
 #include "MQTTClient.h"
 #include "bmc_mqtt.h"
 
-#define LOG_VERSION     "V0.29"
+#define LOG_VERSION     "V0.30"
 #define MQTT_VERSION    "V3.11"
 #define TNAME  "maint9"
 #define LADDRESS        "tcp://127.0.0.1:1883"
